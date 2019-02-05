395ec02ef8d1afde67b0210ced5c763f3adfde30
AJ Rojas <arojas@greenvalecs.com>
2019-02-04 18:21:28 -0800

    Updates

---
+++ precache-manifest.9d8eb1fcfc10462766ad499ee2e10665.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4f30a53787da5ef8c05bf109efb0988",
+    "url": "/clickmemorygame/static/media/joan.f4f30a53.jpg"
+  },
+  {
+    "revision": "904842c2ac1fd37ece32",
+    "url": "/clickmemorygame/static/css/main.3b9731b1.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/clickmemorygame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "b9425c43dbff9852b364",
+    "url": "/clickmemorygame/static/js/runtime~main.b9425c43.js"
+  },
+  {
+    "revision": "c1795043f0131507a211cc665f327896",
+    "url": "/clickmemorygame/static/media/banner.c1795043.jpg"
+  },
+  {
+    "revision": "bcd0cca4d30ceb694b14e547690fdea5",
+    "url": "/clickmemorygame/static/media/draper.bcd0cca4.jpg"
+  },
+  {
+    "revision": "5f5e539261088604a4bdf37e9e2411be",
+    "url": "/clickmemorygame/static/media/draperart.5f5e5392.jpg"
+  },
+  {
+    "revision": "10a411d7eacb9354c771553dada43234",
+    "url": "/clickmemorygame/static/media/fallingpic.10a411d7.jpg"
+  },
+  {
+    "revision": "a720bfbad1b689825b33b5fdeaf614f1",
+    "url": "/clickmemorygame/static/media/framepic.a720bfba.jpg"
+  },
+  {
+    "revision": "904842c2ac1fd37ece32",
+    "url": "/clickmemorygame/static/js/main.904842c2.chunk.js"
+  },
+  {
+    "revision": "038736246ef6512be8b9fd290607c045",
+    "url": "/clickmemorygame/static/media/luckystrike.03873624.jpg"
+  },
+  {
+    "revision": "1310ee7b1b9a7d9cc88af5c0f7da5327",
+    "url": "/clickmemorygame/static/media/martini.1310ee7b.jpg"
+  },
+  {
+    "revision": "ea40c9ae1f4145602c46624ab4961529",
+    "url": "/clickmemorygame/static/media/megan.ea40c9ae.jpg"
+  },
+  {
+    "revision": "025f928cb1555ae8adc1b4543f7d3db8",
+    "url": "/clickmemorygame/static/media/peggy.025f928c.jpg"
+  },
+  {
+    "revision": "b8270d7b12a54390892df95333a71df1",
+    "url": "/clickmemorygame/static/media/pete.b8270d7b.jpg"
+  },
+  {
+    "revision": "dd3688acdf19776658eebe93dcac135d",
+    "url": "/clickmemorygame/static/media/portfolio.dd3688ac.jpg"
+  },
+  {
+    "revision": "ced27d24c72e30738f73d125f2bbaa3d",
+    "url": "/clickmemorygame/static/media/poster.ced27d24.jpg"
+  },
+  {
+    "revision": "c916df2dfe0da6e74f03a903dedbd5f8",
+    "url": "/clickmemorygame/static/media/roger.c916df2d.jpg"
+  },
+  {
+    "revision": "cdb12de2a660642a5d598f1b667a5f39",
+    "url": "/clickmemorygame/index.html"
+  }
+];
